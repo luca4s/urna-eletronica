8dbc9579492546ad23d04b673c453aadcde5d004
Lucas <96322105+luca4s@users.noreply.github.com>
2023-03-28 13:12:55 -0300

    commit

--- apurador.py
+++ apurador.py
@@ -10,11 +10,12 @@ def calc(n, d):
 data = json.loads(f)
 candidatos = data['candidatos']
 eleitores = data['eleitores']
-html = "<!DOCTYPE html> <html> <head> <style> body { font-family: arial, sans-serif; text-align: center; } table { margin-left: auto; margin-right: auto; border-collapse: collapse; width: 75%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } </style> <title>Resultados</title> </head> <body> <h2>Resultados</h2> <table> <tr> <th>Nome</th> <th>Votou?</th> </tr>"
+html = "<!DOCTYPE html> <html> <head> <style> body { font-family: arial, sans-serif; text-align: center; } table { margin-left: auto; margin-right: auto; border-collapse: collapse; width: 75%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } input[type=checkbox][disabled][value=true] { background-color: rbg(0, 117, 255); } </style> <title>Resultados</title> </head> <body> <h2>Resultados</h2> <table> <tr> <th>Nome</th> <th>Votou?</th> </tr>"
 votos = 0
 ausente = 0
 ausentes = []
 votosvalidos = 0
+validoscalc = False
 votosbrancos = candidatos["BRANCO"]["votos"]
 votosnulos = candidatos["NULO"]["votos"]
 candidatomaisvotado = ""
@@ -25,17 +26,19 @@ for eleitor in eleitores:
         html += f" <tr> <td>{eleitores[eleitor]['nome']}</td> <td><input type='checkbox' checked disabled></td> </tr>"
     else:
         html += f" <tr> <td>{eleitores[eleitor]['nome']}</td> <td><input type='checkbox' disabled></td> </tr>"
-html += f" </table> <br> <table> <tr> <th>Votos</th> <th>Votos Válidos</th> <th>Votos em Branco</th> <th>Votos Nulos</th> </tr> <tr> <td>{votos}</td> <td>{votosvalidos} ({calc(votosvalidos, votos)}% dos votos)</td> <td>{votosbrancos} ({calc(votosbrancos, votos)}% dos votos)</td> <td>{votosnulos} ({calc(votosnulos, votos)}% dos votos)</td> </tr> </table> <br> <table> <tr> <th>Número</th> <th>Votos</th> <th>Candidato</th> <th>Vice</th> <th>Partido</th> </tr>"
 print("Apurando votos...")
 for candidato in candidatos:
     if candidato != "BRANCO" and candidato != "NULO":
         votosvalidos += candidatos[candidato]["votos"]
+        if not validoscalc:
+            validoscalc = True
+            html += f" </table> <br> <table> <tr> <th>Votos</th> <th>Votos Válidos</th> <th>Votos em Branco</th> <th>Votos Nulos</th> </tr> <tr> <td>{votos}</td> <td>{votosvalidos} ({calc(votosvalidos, votos)}% dos votos)</td> <td>{votosbrancos} ({calc(votosbrancos, votos)}% dos votos)</td> <td>{votosnulos} ({calc(votosnulos, votos)}% dos votos)</td> </tr> </table> <br> <table> <tr> <th>Número</th> <th>Votos</th> <th>Candidato</th> <th>Vice</th> <th>Partido</th> </tr>"
         if candidatomaisvotado == "":
             candidatomaisvotado = candidatos[candidato]["numero"]
         elif candidatos[candidato]["votos"] > candidatos[candidatomaisvotado]["votos"]:
             candidatomaisvotado = candidatos[candidato]["numero"]
         html += f"<tr> <td>{candidatos[candidato]['numero']}</td> <td>{candidatos[candidato]['votos']} ({calc(candidatos[candidato]['votos'], votosvalidos)}% dos votos válidos)</td> <td>{eleitores[candidatos[candidato]['candidato']]['nome']}</td> <td>{eleitores[candidatos[candidato]['vice']]['nome']}</td> <td>{candidatos[candidato]['partido']}</td> </tr>"
 html += " </table> </body> </html>"
-with open('index.html', 'w') as file:
+with open(f'resultados.html', 'w') as file:
     file.write(html)
-    print("Pronto!")
+    print(f'Pronto! Salvo em "{file.name}".')
